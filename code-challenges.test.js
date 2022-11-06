b7124671605749d0b3ef7c69f9e49a8a123882b6
tabithatrubey <tabithatrubey@outlook.com>
2022-11-06 11:37:48 -0800

    Can't believe we're starting  Week 3

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+// const { it } = require("node:test")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,46 +16,205 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
+describe("mult3", () => {
+    it ("takes in an array of numbers and returns an array with all the numbers multiplied by 3", () => {
+        const numbersArray1 = [6, 7, 8, 9, 10]
+        const expectedOutput1 = [18, 21, 24, 27, 30]
+        const numbersArray2 = [24, 27, 30, 33, 36]
+        const expectedOutput2 = [72, 81, 90, 99, 108]
+        expect(mult3(numbersArray1)).toEqual(expectedOutput1)
+        expect(mult3(numbersArray2)).toEqual(expectedOutput2)
+    })
+    })
 
-// b) Create the function that makes the test pass.
+    // ReferenceError: mult3 is not defined
 
-// --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
+    // Psuedo Code 
+    // Create a function called "mult3"
+    // One parameter - array
+    // Since we want to return the array with the SAME length and make a decision about each element/value inside the original array, we will use .map
+    // Return the array with all of its values multiplied by 3
+
+    const mult3 = (arr) => {
+        return arr.map(value => value * 3)
+    }
+    // Test Suites: 1 passed, 1 total
+    // Tests:       1 passed, 1 total
+
+// --------------------1) Create a function that takes an object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const object1 = { number: 15 }
+// First, we start with Test Driven Development (TDD) to write our function in plain text to run it through Jest, a JS testing framework. Anticpated results: fail, pass, as part of the red-green-refactor.
+describe("evenlyDivisible", () => {
+    it ("takes an object as an argument and decides if the number inside it is evenly divisible by three or not", () => {
+        const object1 = { number: 15 }
+        const objectOutput1 = "15 is divisible by three"
+        const object2 = { number: 0 }
+        const objectOutput2 = "0 is divisible by three"
+        const object3 = { number: -7 }
+        const objectOutput3 = "-7 is not divisible by three"
+        expect(evenlyDivisible(object1)).toEqual(objectOutput1)
+        expect(evenlyDivisible(object2)).toEqual(objectOutput2)
+        expect(evenlyDivisible(object3)).toEqual(objectOutput3)
+    })
+})
+// Reference Error: evenlyDivisible is not defined
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+// note to self -check all syntax before running test, PAY ATTENTION TO THE DETAILS! 
+
+// Psuedo Code:
+// Create a function called evenlyDivisible 
+// Has one parameter - number
+// Use a conditional statement to determine if the number is evenly divisible by three or not
+// If the number is divisible by three
+//     return number is divisble by three
+// If the number is not evenly divisible by three
+//     return number is not divisible by three 
+
+
+const evenlyDivisible = (number) => {
+    if (number % 3 === 0)
+    {return `${number} is evenly divisible by three`}
+    else if (number % 3 != 0) {
+        return `${number} is not evenly divisible by three`
+        }
+    }
+//     console.log(evenlyDivisible(15))
+// I am very stuck on this one. My test keeps failing, but I opened a new window and ran the code and it works prefectly. This is the error code: 
+// evenlyDivisible › takes an object as an argument and decides if the number inside it is evenly divisible by three or not
+
+// expect(received).toEqual(expected) // deep equality
+
+// Expected: "15 is divisible by three"
+// Received: "[object Object] is not evenly divisible by three"
+
+//   37 |         const object3 = { number: -7 }
+//   38 |         const objectOutput3 = "-7 is not divisible by three"
+// > 39 |         expect(evenlyDivisible(object1)).toEqual(objectOutput1)
+//      |                                          ^
+//   40 |         expect(evenlyDivisible(object2)).toEqual(objectOutput2)
+//   41 |         expect(evenlyDivisible(object3)).toEqual(objectOutput3)
+//   42 |     })
+
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+
+
+
+// const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
+// const object2 = { number: 0 }
 // Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
+// const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
 
-// b) Create the function that makes the test pass.
+
+
+
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("onlyCapitals", () => {
+    it("takes in an array of words and returns an array with all the words capitalized.", () => {
+        const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+        const randomOutput1 = ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+        const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+        const randomOutput2 = ["Temperature", "Database", "Chopsticks", "Mango"]
+        expect(onlyCapitals(randomNouns1)).toEqual(randomOutput1)
+        expect(onlyCapitals(randomNouns2)).toEqual(randomOutput2)
+    })
+})
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+// ReferenceError: onlyCapitals is not defined
+// Test Suites: 1 failed, 1 total
+// Tests: 2 failed, 2 total
 
-// b) Create the function that makes the test pass.
+// b) Psuedo Code:
+// Create a function called onlyCapitals
+// Takes in one paramater -an array
+// The function will return ALL the word capitalized 
+    //use .map() to iterate over the array and then .toUpperCase to make each word capitalized in each string.
+    // Use a conditional statement to check if the typeof value strictly equals a string so .toUpperCase can successfully run, since it does not work on arrays
+ //If the value is a string, return the 0 index of the string (the first letter in the string) since we want to capitalize the first letter, not the entire word. Use string interpolation so the first letter is returned, as well.
+//  Then, attach .toUpperCase to capitalize the 0 index
+// Then, attach .slice(1) with string interpolation to return the remainder of the word in a string to make it a complete word.   
+
+const onlyCapitals = (array) => {
+    return array.map(value => {
+        if (typeof value === "string") {
+            return `${value[0].toUpperCase()}${value.slice(1)}`
+        } else {
+            return "This is not valid"
+        }
+    })
+}
+// onlyCapitals(["streetlamp", "potato", "teeth", "conclusion", "nephew"]) === ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 passed, 2 total
+// My test that was supposed to serve as the green portion in the RGR kept failing because I had .slice[0], instead of .slice(0) since I thought I would use [] for index notation.
+
+
+
+
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const vowelTester1 = "learn"
-// Expected output: 1
-const vowelTester2 = "academy"
-// Expected output: 0
-const vowelTester3 = "challenges"
-// Expected output: 2
+describe("firstVowel", () => {
+    it("takes in a string and logs the index of the first vowel", () => {
+        const vowelTester1 = "learn"
+        const output1 = 1
+        const vowelTester2 = "academy"
+        const output2 = 0
+        const vowelTester3 = "challenges"
+        const output3 = 2
+        expect(firstVowel(vowelTester1)).toEqual(output1)
+        expect(firstVowel(vowelTester2)).toEqual(output2)
+        expect(firstVowel(vowelTester3)).toEqual(output3)
+    })
+    
+})
+// // ReferenceError: firstVowel is not defined
+// Test Suites: 1 failed, 1 total
+// Tests:       2 failed, 1 passed, 3 total
 
-// b) Create the function that makes the test pass.
+// // Psuedo Code
+// Create a function called fistVowel
+// Takes in one parameter - string
+// Use a conditional statement to evaluate if the data type is a string
+// If the data type is a string
+    // Return indexof fist vowel
+
+
+
+const firstVowel = (arr) => {
+    return arr.search(/[a,e,i,o,u,A,E,I,O,U]/g)
+    }
+//  firstVowel › takes in a string and logs the index of the first vowel
+
+// expect(received).toEqual(expected) // deep equality
+
+// Expected: 1
+// Received: undefined
+
+//   175 |         const vowelTester3 = "challenges"
+//   176 |         const output3 = 2
+// > 177 |         expect(firstVowel(vowelTester1)).toEqual(output1)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       2 failed, 2 passed, 4 total
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 3 passed, 4 total
+
+// I struggled with this one the longest, but glorious YouTube saved me. I initially was using the .filter() method to search for the value that was set at a vowel by using logical and (value === "a" && value === "e"). I had arr.split() to split the array into individual characters, then tacked on .filter to search for the vowels. I think this could have been a start, a disorganized one, but I wasn't sure how to find the FIRST vowel's index. I used .indexof but would yield undefined results. I looked up .filter() to see if I was using it imporperly and I got an article on the differences between .search and .filter, so I YouTube'd .search, which ultimately helped me with this problem and allow the test to pass green. 
+// We use the .search method instead of the .filter because .search specifically looks for the first match of the index in its condition. This makes much more sense as I felt like I was hitting a wall and didn't know where I could go when using .filter() becuase it would have returned results for ALL vowels. I used a,e,i,o,u,A,E,I,O,U in the event an uppercase vowel existed, even though it wasn't relevant for this speciic question. The syntax is /[ ]/g, in which the first / and last / indicate the start and stop similar to for loops, and g represents global so the search is looking for all occurrences not just the first. Therefore, g isn't actually necessary in this equation (I tested without the g and yieled the same results). Essentially the equation could have looked like this and passed, as well 
+// const firstVowel = (arr) => {
+    // return arr.search(/[a,e,i,o,u]/)
+    // }
--- interview-questions.md
+++ interview-questions.md
@@ -8,50 +8,56 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-Your answer:
+Your answer: The difference between a parameter and an an argument is that a parameter holds the values a function calls on when initially created, and an argument is the value called on outside of the function to invoke it throughout a program. 
 
-Researched answer:
+Researched answer:I typically confuse parameters and arguments. However, parameters are the values inside the function, and the argument is the value determined for the function when it is time to log the function. For example, if we started a function like: 
+const myAge = (number) => {
+    return `You are ${number}
+}
+console.log(myAge(77))
+the parameter would be number and the argument would be 77. You can use arguments to make functions more complex and manipulate the data inside functions to create different results. Parameters are placeholders for data, whereas arguments are the actual data passed into the function. Arguments seem more dynamic than parameters.
+
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-Your answer:
+Your answer: The .map() predefined parameters are value, index, and array. The value is required, whereas as the index and array are optional.
 
-Researched answer:
+Researched answer: .map()'s predefined paramters are current value, index, and array. The current value is the only one that is always required for .map(). The order of these parameters must be continuously maintained, which is why the value is always required. .map MUST take at least one argument, which the first is always value, but they can hold at maximum three arguments. 
 
 3. What is the difference between map and filter?
 
-Your answer:
+Your answer: Map and filter are both great for iteration purposes, however, map iterates through an array and returns an array of the same length, while filer iterates through an array but only returns the conditions it filters for. 
 
-Researched answer:
+Researched answer: One of the biggest differences between map and filter is their execution. Map executes a command for each value in an array and returns an array of the same length. However, filter considers each item in an array, but only extracts the data that meets the specified condition. While they both return a new array, map's length remains the same as the original array, but filter produces a subset of the original array. Filter can also extract different types of data in the original array, while map only modifies and returns the same data type from the original array. 
 
 4. What is iteration?
 
-Your answer:
+Your answer: Iteration is executing a command through a predefined array until a condition is met. The last portion, until a condition is met, is crucial to remember so you don't have a function stopping at a condition much longer or farther than the intended one. Iteration can do something for each value in an array. For example, iteration could take an array of numbers and return the array with all values multiplied by three. 
 
-Researched answer:
+Researched answer: Iteration occurs when we make a decision about every value in an array. Iteration is highlighted in for loops and higher-order functions with .forEach, .map, and .filter. Iteration is dynamic. Iteration can manipulate data and return an array of the same length, or extract only the specified data required to meet the function's conditions. Iteration is extremely useful when a programmer wants to evaluate every element/value in an array or execute a repetetive command.
 
 5. What is the difference between a class and an object?
 
-Your answer:
+Your answer: The difference between a class and an object is a class is a blueprint of an object and an object determines the behavior of data. Classes are static as they have the same exact skeleton for each class, while an object can be dynamic via functions inside the object, called methods.
 
-Researched answer:
+Researched answer: A class is a function that contains data and behavior. An object is an instance of a class. Classes hold properties and act as a shell or blueprint for an object. Classes have certain keywords required to create and access data from within a class. For example, the constructor creates the object inside the class, "this" is used to refer to an object inside the class, and "new" is used when creating a new object (if class = Coffee, then const lattee = new Coffee; latte would now have access to the properties inside of Coffee). A class is written in PascalCase, whereas, objects are written in camelCase. Objects are considered the crossroads between data and behavior because they can hold functions internally, called methods. Objects contain key:value pairs, where key is the data type of symbol and value can be any valid JavaScript data type. An example of this would look like: const person = [name: "Tabi", phone: "9499031938"]; key would be name and phone and value would be "Tabi" and "9499031938". 
 
-6. STRETCH: What is hoisting in JavaScript?
+6. STRETCH: What is hoisting in JavaScript? 
 
-Your answer:
+Your answer: Hoisting in JavaScript is attaching another command onto an invoked fucntion when it is logged.
 
-Researched answer:
+Researched answer: Hoisting allows a programmer to enable a function before it is recorded in their code. Hoisitng happens when the function declaration or variables are transfered to the top of a programmer's scope before the fucntion is invoked.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. User stories:
+1. User stories: A user story describes what the user wants and the expected outcome. The user story allows the developers individuality in their approach on how to achieve the user's expected outcome. We have started to see these in our React series. 
 
-2. Spread operator:
+2. Spread operator: The spread syntax is depicted by (...). The spread operator takes an array or string and expands it in the spot where an argument or no argument at all is anticipated. The spread operator itemizes the object properties and adds the key:value pairs to the new object.
 
-3. React:
+3. React: React is a JavaScript library. React was created by Facebook with the intent to allow developers to construct user interfaces for websites and apps in a quick manner, globally. Everything in React is compromised of components, hence why it is called component-based. Developers combine all their components (small parts) to make one greater application or webpage. React uses JS, CSS, and HTML to offer the most customizable platform for users to interact with. React is also scalable and reusable, which enhances efficiency.  
 
-4. React props:
+4. React props: React props are React properties. Props transfer information from one React component to another. React props allow developers to construct consistent interfaces across React components. React props are accessed via React children in the component hierarchy. 
 
-5. DOM events:
+5. DOM events: The Document Object Model events enable JavaScript to provide event handlers for elements in an HTML document. DOM events  typically operate in conjunction with functions, which creates functionality, like a user clicking on a button. Some DOM events also include MouseEvent, KeyboardEvent, and TouchEvent. 
